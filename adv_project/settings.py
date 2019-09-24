a61254d06140314e2c158b9a8a49d387a9ddda80
christiansallen <christiansallen@gmail.com>
2019-09-25 03:42:24 +0800

    added dummy chatroom. need to put our users and data inside of it

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'adventure',
     'api',
+    'chatroom',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
---
+++ chatroom/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+# import pusher
+
+# channels_client = pusher.Pusher(
+#     app_id='868057',
+#     key='fbd19071de68cf6b0460',
+#     secret='873ddcb23fcaf63476ae',
+#     cluster='us3',
+#     ssl=True
+# )
+
+# channels_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
+# Create your models here.
